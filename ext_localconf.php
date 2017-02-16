b08f3e18371d686b3f2801cf90f5e685e481e61b
Michael Grundkötter <mg@ujamii.com>
2017-02-16 17:36:22 +0100

    - improved require files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,17 @@
 <?php
-
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
 if (!function_exists('register_client')) {
 	function register_client() {
-		$autoloadPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sentry_client') . 'vendor/autoload.php';
+		$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sentry_client');
+		$autoloadPath = $extPath . 'vendor/autoload.php';
 		if (is_readable($autoloadPath)) {
 			require_once $autoloadPath;
 		}
+		require_once $extPath . 'Classes/Client.php';
+
 		$client = new \Lemming\SentryClient\Client();
 		$errorHandler = new Raven_ErrorHandler($client, TRUE);
 		$errorHandler->registerExceptionHandler();
