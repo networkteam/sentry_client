6594a6beaa01f7a3406276e5d0660663b7758149
Torben Hansen <derhansen@gmail.com>
2018-11-11 20:12:20 +0100

    [TASK] TYPO3 9.5 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '8.7.0-8.7.99',
+                    'typo3' => '8.7.0-9.5.99',
                 ],
             'conflicts' => [],
             'suggests' => []
