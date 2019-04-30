5ee2f0a609d4392a5f6f470e5561e5dabb58cfcb
Christoph Lehmann <lehmann@networkteam.com>
2019-04-30 10:56:48 +0200

    Task: Do not clear cache on load and cleanup ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,11 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['sentry_client'] = [
     'title' => 'Sentry Client',
     'description' => 'Sentry Client for TYPO3 - https://www.getsentry.com/',
     'category' => 'services',
     'version' => '2.0.3',
     'state' => 'stable',
-    'uploadfolder' => false,
-    'createDirs' => '',
-    'clearcacheonload' => true,
     'author' => 'Christoph Lehmann',
     'author_email' => 'christoph.lehmann@networkteam.com',
     'author_company' => 'networkteam GmbH',
@@ -18,8 +15,5 @@ $EM_CONF[$_EXTKEY] = [
                 [
                     'typo3' => '8.7.0-9.5.99',
                 ],
-            'conflicts' => [],
-            'suggests' => []
         ],
 ];
-
