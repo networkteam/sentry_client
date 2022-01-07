09dca947b67891bc4471e1c1d022a9993f66085b
Christoph Lehmann <lehmann@networkteam.com>
2022-01-08 00:44:02 +0100

    Task: Catch Allowed memory size exhausted error
    Sentry needs to be initialized before the error occurs to be able to report it.
    
    ExtensionManagementUtility::extPath() can't be used to find the autoload file at this early stage.

--- Classes/DebugExceptionHandler.php
+++ Classes/DebugExceptionHandler.php
@@ -4,6 +4,12 @@ namespace Networkteam\SentryClient;
 
 class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler
 {
+    public function __construct()
+    {
+        parent::__construct();
+        Client::init();
+    }
+
     /**
      * @param \Throwable $exception The throwable object.
      * @throws \Exception
