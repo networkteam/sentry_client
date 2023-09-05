434c4536da4940be45f3a9677ca7d40551497af2
Christoph Lehmann <lehmann@networkteam.com>
2023-09-05 20:57:00 +0200

    Revert "Drop DebugExceptionHandler"
    This reverts commit 8c5e2430d33429f3bb817fc06f27eb7fb885b4e6.

---
+++ Classes/DebugExceptionHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Networkteam\SentryClient;
+
+class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler
+{
+    /**
+     * @param \Throwable $exception The throwable object.
+     * @throws \Exception
+     */
+    public function handleException(\Throwable $exception): void
+    {
+        Client::captureException($exception);
+        parent::handleException($exception);
+    }
+}
