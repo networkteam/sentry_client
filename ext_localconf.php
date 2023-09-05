2ec7c89c4e078095b739736c8ba10d1626683753
Christoph Lehmann <lehmann@networkteam.com>
2023-09-05 22:28:31 +0200

    Configure content exception handler only when enabled

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,8 @@
 
 defined('TYPO3') or die();
 
-call_user_func(function() {
+if (\Networkteam\SentryClient\Service\SentryService::isEnabled()) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\ContentObject\Exception\ProductionExceptionHandler::class] = [
         'className' => \Networkteam\SentryClient\Content\ProductionExceptionHandler::class
     ];
-});
+}
